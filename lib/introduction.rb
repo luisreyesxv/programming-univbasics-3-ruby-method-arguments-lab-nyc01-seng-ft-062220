d2fc84ea977f9d6aecd9827e77a3b7a65c81276b
Luis Reyes <luisreyesxv@yahoo.com>
2020-06-06 04:56:56 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -12,7 +12,7 @@ def introduction_with_language (name,language)
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 
-def introduction_with_language_optional (name,language = "Ruby")
+def introduction_with_language_optional (name,language = "Ruby")  
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 
@@ -103,4 +103,3 @@ end
 #def introduction_with_language_optional(name, language="Ruby")
 #  puts "Hi, my name is #{name} and I am learning to program in #{language}."
 #end
-s
